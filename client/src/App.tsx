bc5b439b43fa8a85d608dc24b83f76ba24b33908
Chuks Joshua <chuksmbanasoj@gmail.com>
2022-12-08 11:11:04 +0100

    Updated Changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { Navbar, Sidebar } from "./components";
+import { Login, Home, SinglePage } from "./pages";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="px-2">
-      <div>App</div>
+    <div>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Amiri:ital@1&family=Lobster+Two&family=Cormorant+Garamond:wght@300&family=Racing+Sans+One&family=Rajdhani:wght@500&family=Roboto+Mono:wght@100&display=swap"
+        rel="stylesheet"
+      ></link>
+      <Router>
+        <Navbar />
+        {/* {isOpen && <Sidebar />} */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Login />} />
+          <Route path="/hotel/:id" element={<SinglePage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
