3c0c79b5faabfdf28dce114100b0059e357fd727
Chuck Joshua <chuksjoshuaa@gmail.com>
2022-12-12 21:47:49 +0100

    .

--- server/dist/src/appDataSource.js
+++ server/dist/src/appDataSource.js
@@ -20,6 +20,6 @@ exports.dataSource = new typeorm_1.DataSource({
     synchronize: !constant_1.__prod__,
     logging: !constant_1.__prod__,
     migrations: [path_1.default.join(__dirname, "./migrations/*")],
-    entities: ["dist/**/**.entity{.js}"],
+    entities: ["dist/**/**.entity.{ts,js}"],
 });
 //# sourceMappingURL=appDataSource.js.map
