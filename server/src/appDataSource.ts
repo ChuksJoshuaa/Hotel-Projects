0e3dabc142875fcb70af5ced4f0da28daf8b0ddf
D-coder <chuksjoshuaa@gmail.com>
2022-12-12 21:06:29 +0100

    WIP: Fixed Minor Bugs

--- server/src/appDataSource.ts
+++ server/src/appDataSource.ts
@@ -3,9 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 import path from "path";
 import { __prod__ } from "./constant";
-// import { Hotel } from "./entities/Hotel.entity";
-// import { HotelBrand } from "./entities/HotelBrand.entity";
-// import { User } from "./entities/User.entity";
 
 const portNumber = Number(process.env.DATABASE_PORT);
 
@@ -19,5 +16,5 @@ export const dataSource = new DataSource({
   synchronize: !__prod__,
   logging: !__prod__,
   migrations: [path.join(__dirname, "./migrations/*")],
-  entities: ["dist/**/**.entity{.ts,.js}", "src/**/**.entity{.ts,.js}"],
+  entities: ["dist/src/**/**.entity{.ts,.js}", "src/**/**.entity{.ts,.js}"],
 });
