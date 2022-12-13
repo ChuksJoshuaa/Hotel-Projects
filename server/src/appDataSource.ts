040480653af15550a7e02d6f5627697945912999
Chuks Joshua <chuksmbanasoj@gmail.com>
2022-12-13 11:26:59 +0100

    WIP: FIxed Minor Bugs

--- server/src/appDataSource.ts
+++ server/src/appDataSource.ts
@@ -3,19 +3,18 @@ import dotenv from "dotenv";
 dotenv.config();
 import path from "path";
 import { __prod__ } from "./constant";
-// import { Hotel } from "./entities/Hotel.entity";
-// import { HotelBrand } from "./entities/HotelBrand.entity";
-// import { User } from "./entities/User.entity";
+import { Hotel } from "./entities/Hotel.entity";
+import { HotelBrand } from "./entities/HotelBrand.entity";
+import { User } from "./entities/User.entity";
 
 const portNumber = Number(process.env.DATABASE_PORT);
-const isCompiled = path.extname(__filename).includes("js");
 
 export const dataSource = new DataSource({
   type: "postgres",
   url: process.env.DATABASE_URL,
   port: portNumber,
   synchronize: !__prod__,
-  entities: [`src/entities/**/*.${isCompiled ? "js" : "ts"}`],
+  entities: [Hotel, HotelBrand, User],
   logging: !__prod__,
   migrations: [path.join(__dirname, "./migrations/*")],
 });
