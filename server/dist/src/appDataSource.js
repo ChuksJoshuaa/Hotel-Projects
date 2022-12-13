a9440452e8d2fce6a89889e359573b35545a6693
Chuks Joshua <chuksmbanasoj@gmail.com>
2022-12-13 10:57:30 +0100

    WIP: FIxed Minor Bugs

--- server/dist/src/appDataSource.js
+++ server/dist/src/appDataSource.js
@@ -9,16 +9,13 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const path_1 = __importDefault(require("path"));
 const constant_1 = require("./constant");
-const Hotel_entity_1 = require("./entities/Hotel.entity");
-const HotelBrand_entity_1 = require("./entities/HotelBrand.entity");
-const User_entity_1 = require("./entities/User.entity");
 const portNumber = Number(process.env.DATABASE_PORT);
 exports.dataSource = new typeorm_1.DataSource({
     type: "postgres",
     url: process.env.DATABASE_URL,
     port: portNumber,
     synchronize: !constant_1.__prod__,
-    entities: [Hotel_entity_1.Hotel, HotelBrand_entity_1.HotelBrand, User_entity_1.User],
+    entities: [path_1.default.join(__dirname, "./entities/*")],
     logging: !constant_1.__prod__,
     migrations: [path_1.default.join(__dirname, "./migrations/*")],
 });
