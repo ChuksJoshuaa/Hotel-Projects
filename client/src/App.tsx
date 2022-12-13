ae1afcf96faaf3fe0803aeeddce4f3b67a67b206
Chuks Joshua <chuksmbanasoj@gmail.com>
2022-12-13 08:24:44 +0100

    WIP: FIxed Minor Bugs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -34,7 +34,7 @@ const cache = new InMemoryCache({
 });
 
 const client = new ApolloClient({
-  uri: "https://hotel-graphql.vercel.app/graphql",
+  uri: "http://localhost:5000/graphql",
   cache,
   credentials: "include",
 });
